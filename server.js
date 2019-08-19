7472b7d39316de8fa5c00de61bb13ad0d06022c9
Jon Struve <ocdammo3@gmail.com>
2019-08-19 16:33:16 -0500

    Added server.js and json files

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/location', (request, response) => {
+    try{
+        const geoData = require('./geo.json')
+        const location = new Location(request.query.location, geoData)
+        response.send(location);
+    } catch(error){
+        response.status(500).send('Sorry, something went wrong.')
+    }
+})
+
+function Location(query, geoData){
+    this.search_query = query;
+    this.formatted_query = geoData.results[0].formatted_address;
+    this.latitude = geoData.results[0].geometry.location.lat;
+    this.longitude = geoData.results[0].geometry.location.lng;
+}
+
+app.get('/weather', (request, response) => {
+    response.send('also works');
+})
+
+app.listen(3000, () => {
+    console.log('server is listening');
+})
